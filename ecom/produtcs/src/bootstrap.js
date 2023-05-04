ae4fb089da4ad47e0d87785e1c5a5b70d601ac3c
mohammedswaaf <mohammedswaaf@gmail.com>
2023-05-04 19:31:03 +0300

    Products app

---
+++ ecom/produtcs/src/bootstrap.js
@@ -0,0 +1,8 @@
+import faker from 'faker';
+let products = '';
+for (let index = 0; index < 10; index++) {
+    const name = faker.commerce.productName();
+    products += `<div> ${name}</div>`
+
+}
+document.querySelector('#devProducts').innerHTML = products;
